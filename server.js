1f50a8b6d1d2174313b40516c77cc69387e3d5d2
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-12 15:48:45 +0000

    adds creat new song

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ var parser = require('body-parser');
 var app = express();
 var path = require('path');
 
-app.use(parser.json);
+
+app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static('client/build'));
 
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/christmas_songs', function(err, database){
+MongoClient.connect('mongodb://localhost:27017/christmas_songs', function(err, client){
   if(err){
     return console.log(err);
   }
 
-  db = database;
+  db = client.db('christmas_songs');
   console.log("Connected to DB");
 
   app.listen(3000, function(){
@@ -24,4 +25,23 @@ MongoClient.connect('mongodb://localhost:27017/christmas_songs', function(err, d
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/build/index.html');
+});
+
+app.get('/songs', function(req, res){
+  db.collection('christmas_songs').find().toArray(function(err, results){
+    if(err){
+      return console.log(err);
+    }
+    res.json(results);
+  });
+});
+
+app.post('/songs', function(req, res){
+  db.collection('christmas_songs').save(req.body, function(err, result){
+    if(err){
+      return console.log(err);
+    }
+    console.log("Saved to database");
+    res.redirect('/');
+  });
 });
