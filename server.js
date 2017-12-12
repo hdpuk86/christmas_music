6a5949f7e3b567081e99d11b0921151d9aab3f6d
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-12 15:08:46 +0000

    completes setup

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var parser = require('body-parser');
+var app = express();
+var path = require('path');
+
+app.use(parser.json);
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static('client/build'));
+
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/christmas_songs', function(err, database){
+  if(err){
+    return console.log(err);
+  }
+
+  db = database;
+  console.log("Connected to DB");
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/client/build/index.html');
+});
